25401cee0ed6a831f74ba5c86179e01d7767c2e2
sallyyeng@gmail.com <SALLYYENG@GMAIL.COM>
2018-01-20 15:53:30 -0800

    Refactor evenOccurence

--- evenOccurrence.js
+++ evenOccurrence.js
@@ -10,33 +10,26 @@
  * console.log(onlyEven); //  4
 */
 
-// const evenOccurrence = (arr) => {
-//   // initialize counter object
-//   let counterObj = {};
+//******** Solution using binaries ********//
 
-//   // iterate through array
-//   arr.forEach(value => {
-//     if (counterObj[value]) {
-//       // if key of value exists, incremement
-//       counterObj[value]++;
-//     } else {
-//       // else initialize to one
-//       counterObj[value] = 1;
-//     }
-//   });
+const evenOccurrence = (arr) => {
+  let counterObj = {};
 
-//   // iterate through array
-//   for (let i = 0; i < arr.length; i++) {
-//     // if key's value is even in counter
-//     if (counterObj[arr[i]] % 2 === 0) {
-//       console.log(arr[i]);
-//       return arr[i];
-//     }
-//   }
-//   return null;
-// };
+  // iterate through array and switch each occurrence of a value between true and false
+  arr.forEach(value => {
+    counterObj[value] = !counterObj[value];
+  });
+  // return the first false occurrence
+  for (let i = 0; i < arr.length; i++) {
+    if (!counterObj[arr[i]]) {
+      console.log(arr[i]);
+      return arr[i];
+    }
+  }
+  return null;
+};
 
-//******** Solution using binaries instead ********//
+//******** Solution not using binaries ********//
 
 // const evenOccurrence = (arr) => {
 //   // initialize counter object
@@ -44,23 +37,16 @@
 
 //   // iterate through array
 //   arr.forEach(value => {
-//     if (counterObj[value]) {
-//       // if key of value exists, incremement
-//       counterObj[value] = false;
-//     } else {
-//       // else initialize to one
-//       counterObj[value] = !counterObj[value];
-//     }
+//     // if key of value exists, incremement, else initialize to one
+//     counterObj[value] = counterObj[value] ? counterObj[value] += 1 : 1;
 //   });
 
 //   // iterate through array
 //   for (let i = 0; i < arr.length; i++) {
 //     // if key's value is even in counter
 //     if (counterObj[arr[i]] % 2 === 0) {
-//       console.log(arr[i]);
 //       return arr[i];
 //     }
 //   }
 //   return null;
 // };
-
