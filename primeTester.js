1085e1b7c677131fadd5e6b5b31964408eeb14d4
sallyyeng@gmail.com <SALLYYENG@GMAIL.COM>
2018-01-22 18:19:15 -0800

    Complete primeTester

---
+++ primeTester.js
@@ -0,0 +1,25 @@
+/**
+ * A prime number is a whole number that has no other divisors other than
+ * itself and 1. Write a function that accepts a number and returns true if it's
+ * a prime number, false if it's not.
+ */
+
+const primeTester = (n) => {
+  if (typeof n !== 'number' || n < 1 || n % 1 !== 0) {
+    // if n isn't a number or n is less than 1 or n is not an integer
+    return false;
+  }
+
+  // get squareroot of value
+  const upperLimit = Math.sqrt(Math.abs(n));
+
+  for (let i = 2; i < upperLimit; i++) {
+    // if value is divisible by any value between 2 and upperlimit, return false
+    if (n % i === 0) { return false; }
+  }
+
+  // if passed all tests, return true
+  return true;
+};
+
+primeTester(17);
