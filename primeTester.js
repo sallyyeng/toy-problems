7f4574e4e7ae076ddd2f685ec014033849b5929e
sallyyeng@gmail.com <SALLYYENG@GMAIL.COM>
2018-01-26 18:34:00 -0800

    Refactor to account for 1

--- primeTester.js
+++ primeTester.js
@@ -5,7 +5,7 @@
  */
 
 const primeTester = (n) => {
-  if (typeof n !== 'number' || n < 1 || n % 1 !== 0) {
+  if (typeof n !== 'number' || n < 1 || n % 1 !== 0 || n === 1) {
     // if n isn't a number or n is less than 1 or n is not an integer
     return false;
   }
@@ -13,13 +13,13 @@ const primeTester = (n) => {
   // get squareroot of value
   const upperLimit = Math.sqrt(Math.abs(n));
 
-  for (let i = 2; i < upperLimit; i++) {
+  for (let i = 2; i <= upperLimit; i++) {
     // if value is divisible by any value between 2 and upperlimit, return false
-    if (n % i === 0) { return false; }
+    if (n % i === 0) {
+      return false;
+    }
   }
 
   // if passed all tests, return true
   return true;
 };
-
-primeTester(17);
