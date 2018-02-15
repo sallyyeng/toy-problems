3f1214975fe064385d39330727e036f2e4e550e9
sallyyeng@gmail.com <SALLYYENG@GMAIL.COM>
2018-02-14 22:40:03 -0800

    Refactor longestPalindrome

--- longestPalindrome.js
+++ longestPalindrome.js
@@ -6,38 +6,26 @@
 * whitespace on each side of dad).
 */
 
-var longestPalindrome = function (string) {
+const longestPalindrome = function(string) {
+  let length = string.length;
+  let longestSoFar = '';
 
-  let stringArr = string.toLowerCase().split('');
-  longestPal = '';
+  for (let i = 0; i < length; i++) {
+    let evenCase = findPal(i, i + 1, length, string);
+    let oddCase = findPal(i - 1, i + 1, length, string);
+    longestSoFar = evenCase.length > longestSoFar.length ? evenCase : longestSoFar;
+    longestSoFar = oddCase.length > longestSoFar.length ? oddCase : longestSoFar;
+  }
 
-  stringArr.forEach((letter, i) => {
-    let currPal = [stringArr[i]];
-    let rightPos = 1;
-    let leftPos = 1;
-
-    if (letter === stringArr[i + rightPos]) {
-      currPal.push(stringArr[i + rightPos]);
-      rightPos = 2;
-    }
-
-    while (stringArr[i - leftPos] && stringArr[i - leftPos] === stringArr[i + rightPos]) {
-      let equalLetter = stringArr[i + rightPos];
-      currPal.push(equalLetter);
-      currPal.unshift(equalLetter);
-
-      rightPos += 1;
-      leftPos += 1;
-    }
-
-    if (currPal.length > longestPal.length) {
-      longestPal = currPal;
-    }
-
-  });
-  console.log(longestPal.join(''));
-  return longestPal.join('');
+  return longestSoFar;
 };
 
-longestPalindrome('Daad is a raceecar a');
-
+const findPal = (leftIndex, rightIndex, length, string) => {
+  while ( leftIndex >= 0 && rightIndex < length && string[leftIndex] === string[rightIndex]) {
+    leftIndex--;
+    rightIndex++;
+  }
+  // left and right will go one index further than palindrome
+  // you want to increment left and slice doesn't include the second argument index (just goes up to)
+  return string.slice(leftIndex + 1, rightIndex);
+};
