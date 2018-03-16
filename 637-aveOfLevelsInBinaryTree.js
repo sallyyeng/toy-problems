6b9adad29da6b04adedf7e5d855aad8f0432c5a5
sallyyeng <sallyyeng@gmail.com>
2018-03-16 01:36:17 -0700

    complete averageOfLevelsInBinaryTree

---
+++ 637-aveOfLevelsInBinaryTree.js
@@ -0,0 +1,78 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+
+const BinaryTreeNode = function(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
+};
+
+BinaryTreeNode.prototype.insertLeft = function(value) {
+  this.left = new BinaryTreeNode(value);
+  return this.left;
+};
+
+BinaryTreeNode.prototype.insertRight = function(value) {
+  this.right = new BinaryTreeNode(value);
+  return this.right;
+};
+
+const Queue = function() {
+  this.storage = {};
+  this.start = 0;
+  this.end = 0;
+};
+
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.end++] = value;
+};
+
+Queue.prototype.dequeue = function() {
+  let result = this.storage[this.start];
+  delete this.storage[this.start];
+  if (this.size) { this.start++; }
+  return result;
+};
+
+Queue.prototype.size = function() {
+  return this.end - this.start;
+};
+
+const averageOfLevels = function(root) {
+  let queue = new Queue();
+  let previousDepth = 0;
+  let sum = 0;
+  let count = 0;
+  let result = [];
+  let current = [root, 0];
+
+  while (current) {
+    [node, depth] = current;
+
+    // handle average tracking
+    if (depth !== previousDepth) {
+      console.log(typeof (sum / count));
+      result.push(sum / count);
+      sum = 0;
+      count = 0;
+      previousDepth++;
+    }
+    sum += node.value;
+    count++;
+
+    node.left ? queue.enqueue([node.left, depth + 1]) : null;
+    node.right ? queue.enqueue([node.right, depth + 1]) : null;
+    current = queue.dequeue();
+  }
+  result.push(sum / count);
+  return result;
+};
